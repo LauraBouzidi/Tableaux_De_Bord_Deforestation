75fecb06f91758dca614ebe3a576424a3c917e76
Laura Bouzidi <laura.bouzidi@outlook.fr>
2019-02-08 11:47:39 +0100

    Clean sans liste

--- Traitement/clean_text.py
+++ Traitement/clean_text.py
@@ -23,13 +23,11 @@ def sentense2cleanTokens(sent):
     sent = " ".join(sent.split())
     return sent
 
-
-sent = "Larger gains from improved management over sparing\\u2013sharing for tropical forests"
 def clean_words (sent):
     stop_words = set(stopwords.words('english'))
     word_tokens = word_tokenize(sent)
     filtered_sentence = [w for w in word_tokens if w not in stop_words]
-    clean_sentence = ""
+    clean_sentence = []
     for sent2 in filtered_sentence:
         if sentense2cleanTokens(sent2) != "":
             clean_sentence.append(sentense2cleanTokens(sent2))
@@ -37,7 +35,7 @@ def clean_words (sent):
 
 path = "C:/Users/laura/Documents/M1/Tableau de bord/Articles_Natura/Articles_Natura/"
 path2 = "C:/Users/laura/Documents/M1/Tableau de bord/Articles_Natura/Clean_Articles_Natura/"
-
+doc = 'Natura_0.txt'
 
 for doc in os.listdir(path):
     fichier = open(path + doc, encoding = "utf_8")
@@ -46,6 +44,14 @@ for doc in os.listdir(path):
     dic = yaml.load(text)
     dic['title']=clean_words(dic['title'])
     dic['content']=clean_words(dic['content'])
+    cont = ''
+    title = ''
+    for i in dic['content']:
+        cont = cont + " " + i
+    for j in dic['title']:
+        title = title + " " + i
+    dic['content'] = cont
+    dic['title'] = title
     fichier1 = open(path2 + "clean_" + doc, "a", encoding = "utf_8")
     fichier1.write(str(dic))
     fichier1.close()
