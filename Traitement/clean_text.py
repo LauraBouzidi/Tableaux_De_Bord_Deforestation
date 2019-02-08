b1f22cee84e7490d851c8a0de2e7f9233ccdeb6b
Laura Bouzidi <laura.bouzidi@outlook.fr>
2019-02-08 11:04:17 +0100

    Création fichiers + Clean code + Clean Data

---
+++ Traitement/clean_text.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  8 10:07:48 2019
+
+@author: laura
+"""
+
+
+import random
+import nltk
+import numpy as np
+from collections import defaultdict
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import os
+import yaml
+
+def sentense2cleanTokens(sent):
+    #sent = sent.lower()
+    sent = "".join([x if x.isalnum() else "" for x in sent])
+    sent = " ".join(sent.split())
+    return sent
+
+
+sent = "Larger gains from improved management over sparing\\u2013sharing for tropical forests"
+def clean_words (sent):
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(sent)
+    filtered_sentence = [w for w in word_tokens if w not in stop_words]
+    clean_sentence = ""
+    for sent2 in filtered_sentence:
+        if sentense2cleanTokens(sent2) != "":
+            clean_sentence.append(sentense2cleanTokens(sent2))
+    return(clean_sentence)
+
+path = "C:/Users/laura/Documents/M1/Tableau de bord/Articles_Natura/Articles_Natura/"
+path2 = "C:/Users/laura/Documents/M1/Tableau de bord/Articles_Natura/Clean_Articles_Natura/"
+
+
+for doc in os.listdir(path):
+    fichier = open(path + doc, encoding = "utf_8")
+    text = fichier.read()
+    fichier.close()
+    dic = yaml.load(text)
+    dic['title']=clean_words(dic['title'])
+    dic['content']=clean_words(dic['content'])
+    fichier1 = open(path2 + "clean_" + doc, "a", encoding = "utf_8")
+    fichier1.write(str(dic))
+    fichier1.close()
